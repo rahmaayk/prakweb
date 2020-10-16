8225b66edf82bdf3fad17984cfc8dae64bdfaabb
rahmaayk <97rahmayk@gmail.com>
2020-10-17 02:31:22 +0700

    web lanjut

---
+++ app/Controllers/Home.php
@@ -0,0 +1,20 @@
+<?php namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function show()
+	{
+		$data['nama'] = 'Rahmaa';
+		$data['jurusan'] = 'Ilmu Komputer';
+		echo view('mahasiswa/header', $data);
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer', $data)
+	}
+}
